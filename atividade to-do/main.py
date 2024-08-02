414a65970bf3c7176011be27984063bf09dcf129
riberokk <pedro.rib08@gmail.com>
2024-08-02 09:53:22 -0300

    atividade to-do

--- atividade to-do/main.py
+++ atividade to-do/main.py
@@ -1,5 +1,23 @@
 from functions import *
 
 while True:
-    
+    os.system("cls")
+    print("/-------------------------/")
+    print("|[1] - Visualizar Tarefas |")
+    print("|[2] - Criar Tarefas      |")
+    print("|[3] - Editar Tarefas     |")
+    print("|[4] - Excluir Tarefas    |")
+    print("|-------------------------/")
+    menu = int(input("--> "))
 
+    if menu == 1:
+        visualizar()
+
+    if menu == 2:
+        criar()
+
+    if menu == 3:
+        editar()
+
+    if menu == 4:
+        excluir()
